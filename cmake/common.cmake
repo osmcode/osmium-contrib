c8c6a805a36d99c383be6c526017dc70f99d924e
Jochen Topf <jochen@topf.org>
2014-12-17 17:57:36 +0100

    Add 'Dev' build type. Remove need for OsmiumOptions.cmake.

--- cmake/common.cmake
+++ cmake/common.cmake
@@ -4,8 +4,6 @@
 #
 #----------------------------------------------------------------------
 
-include(OsmiumOptions)
-
 include(CppcheckTarget)
 add_cppcheck_target(*.*pp)
 
@@ -26,7 +24,25 @@ add_definitions(-std=${USE_CPP_VERSION})
 
 #----------------------------------------------------------------------
 
-if(NOT MSVC)
-    add_definitions(${OSMIUM_WARNING_OPTIONS})
-endif()
+set(CMAKE_CXX_FLAGS_NONE "-O3 -g"
+    CACHE STRING "Flags used by the compiler during all builds."
+    FORCE)
+
+set(CMAKE_CXX_FLAGS_DEV "-O3 -g ${OSMIUM_WARNING_OPTIONS}"
+    CACHE STRING "Flags used by the compiler during developer builds."
+    FORCE)
+set(CMAKE_EXE_LINKER_FLAGS_DEV ""
+    CACHE STRING "Flags used by the linker during developer builds."
+    FORCE)
+mark_as_advanced(
+    CMAKE_CXX_FLAGS_DEV
+    CMAKE_EXE_LINKER_FLAGS_DEV
+)
+
+set(CMAKE_CONFIGURATION_TYPES "Debug Release RelWithDebInfo MinSizeRel Dev")
+
+# Update the documentation string of CMAKE_BUILD_TYPE for GUIs
+set(CMAKE_BUILD_TYPE "${CMAKE_BUILD_TYPE}" CACHE STRING
+    "Choose the type of build, options are: ${CMAKE_CONFIGURATION_TYPES}."
+    FORCE)
 
