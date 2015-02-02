9e61891c27792836e621ca9fc21eca842a5e15aa
Jochen Topf <jochen@topf.org>
2015-02-02 09:13:22 +0100

    Update CMake config. Incorporate changes from other repositories.

--- cmake/common.cmake
+++ cmake/common.cmake
@@ -1,36 +1,51 @@
-#----------------------------------------------------------------------
+#-----------------------------------------------------------------------------
 #
 #  CMake config used in all projects in this repository.
 #
-#----------------------------------------------------------------------
+#-----------------------------------------------------------------------------
 
 include(CppcheckTarget)
 add_cppcheck_target(*.*pp)
 
-#----------------------------------------------------------------------
+
+#-----------------------------------------------------------------------------
 #
 #  Decide which C++ version to use (Minimum/default: C++11).
 #
-#----------------------------------------------------------------------
+#-----------------------------------------------------------------------------
 
-if(NOT USE_CPP_VERSION)
-    set(USE_CPP_VERSION c++11)
+if(NOT MSVC)
+    if(NOT USE_CPP_VERSION)
+        set(USE_CPP_VERSION c++11)
+    endif()
+    message(STATUS "Use C++ version: ${USE_CPP_VERSION}")
+    # following only available from cmake 2.8.12:
+    #   add_compile_options(-std=${USE_CPP_VERSION})
+    # so using this instead:
+    add_definitions(-std=${USE_CPP_VERSION})
 endif()
-message(STATUS "Use C++ version: ${USE_CPP_VERSION}")
-# following only available from cmake 2.8.12:
-#   add_compile_options(-std=${USE_CPP_VERSION})
-# so using this instead:
-add_definitions(-std=${USE_CPP_VERSION})
 
-#----------------------------------------------------------------------
 
-set(CMAKE_CXX_FLAGS_NONE "-O3 -g"
-    CACHE STRING "Flags used by the compiler during all builds."
-    FORCE)
+#-----------------------------------------------------------------------------
+#
+#  Compiler and Linker flags
+#
+#-----------------------------------------------------------------------------
+if(MSVC)
+    set(USUAL_COMPILE_OPTIONS "/Ox")
+else()
+    set(USUAL_COMPILE_OPTIONS "-O3 -g")
+endif()
 
-set(CMAKE_CXX_FLAGS_DEV "-O3 -g ${OSMIUM_WARNING_OPTIONS}"
+if(WIN32)
+    add_definitions(-DWIN32 -D_WIN32 -DMSWIN32 -DBGDWIN32
+                    -DWINVER=0x0500 -D_WIN32_WINNT=0x0500 -D_WIN32_IE=0x0600)
+endif()
+
+set(CMAKE_CXX_FLAGS_DEV "${USUAL_COMPILE_OPTIONS}"
     CACHE STRING "Flags used by the compiler during developer builds."
     FORCE)
+
 set(CMAKE_EXE_LINKER_FLAGS_DEV ""
     CACHE STRING "Flags used by the linker during developer builds."
     FORCE)
@@ -39,10 +54,37 @@ mark_as_advanced(
     CMAKE_EXE_LINKER_FLAGS_DEV
 )
 
+set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "${USUAL_COMPILE_OPTIONS}"
+    CACHE STRING "Flags used by the compiler during RELWITHDEBINFO builds."
+    FORCE)
+
+
+#-----------------------------------------------------------------------------
+#
+#  Build Type
+#
+#-----------------------------------------------------------------------------
 set(CMAKE_CONFIGURATION_TYPES "Debug Release RelWithDebInfo MinSizeRel Dev")
 
-# Update the documentation string of CMAKE_BUILD_TYPE for GUIs
-set(CMAKE_BUILD_TYPE "${CMAKE_BUILD_TYPE}" CACHE STRING
+# In 'Dev' mode: compile with very strict warnings and turn them into errors.
+if(CMAKE_BUILD_TYPE STREQUAL "Dev")
+    if(NOT MSVC)
+        add_definitions(-Werror)
+    endif()
+    add_definitions(${OSMIUM_WARNING_OPTIONS})
+endif()
+
+# Force RelWithDebInfo build type if none was given
+if(CMAKE_BUILD_TYPE)
+    set(build_type ${CMAKE_BUILD_TYPE})
+else()
+    set(build_type "RelWithDebInfo")
+endif()
+
+set(CMAKE_BUILD_TYPE ${build_type}
+    CACHE STRING
     "Choose the type of build, options are: ${CMAKE_CONFIGURATION_TYPES}."
     FORCE)
 
+
+#-----------------------------------------------------------------------------
