b80e8649b8f2d2da228f0c9cb1cc471b3fd16292
Jochen Topf <jochen@topf.org>
2015-04-16 11:14:59 +0200

    Cleanup travis
    * Use same workaround for GCC bug as in libosmium
    * Output from ctest only on failure
    * General cleanup

--- cmake/build_subprojects.sh
+++ cmake/build_subprojects.sh
@@ -4,14 +4,15 @@ set -x
 set -e
 
 CONFIGURATION=$1
+shift
 
 for project in amenity_list export_to_wkt mapolution node_density pub_names road_length; do
     cd $project
     mkdir -p build
     cd build
-    cmake -LA -DCMAKE_BUILD_TYPE=${CONFIGURATION} ..
+    cmake -LA -DCMAKE_BUILD_TYPE=${CONFIGURATION} $* ..
     make VERBOSE=1
-    ctest -V
+    ctest --output-on-failure
     cd ../..
 done
 
