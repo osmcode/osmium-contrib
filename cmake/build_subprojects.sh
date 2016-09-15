4ccbcd072da6fe58f4f181c93708ab0f2a63f823
Jochen Topf <jochen@topf.org>
2016-09-15 18:20:46 +0200

    Remove removed projects from project list.

--- cmake/build_subprojects.sh
+++ cmake/build_subprojects.sh
@@ -6,7 +6,7 @@ set -e
 CONFIGURATION=$1
 shift
 
-for project in amenity_list export_to_wkt mapolution node_density pub_names road_length; do
+for project in amenity_list export_to_wkt mapolution node_density; do
     cd $project
     mkdir -p build
     cd build
