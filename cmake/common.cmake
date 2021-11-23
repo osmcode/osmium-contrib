c3d6c726125c1e47d5555c89fe320239bb09f071
Jochen Topf <jochen@topf.org>
2021-11-23 10:26:59 +0100

    Do not C++ standard when compiling, use compiler default
    This is not needed any more now that most compilers people will
    encounter use C++11 or newer by default.

--- cmake/common.cmake
+++ cmake/common.cmake
@@ -6,24 +6,6 @@
 
 #-----------------------------------------------------------------------------
 #
-#  Decide which C++ version to use (Minimum/default: C++11).
-#
-#-----------------------------------------------------------------------------
-
-if(NOT MSVC)
-    if(NOT USE_CPP_VERSION)
-        set(USE_CPP_VERSION c++11)
-    endif()
-    message(STATUS "Use C++ version: ${USE_CPP_VERSION}")
-    # following only available from cmake 2.8.12:
-    #   add_compile_options(-std=${USE_CPP_VERSION})
-    # so using this instead:
-    add_definitions(-std=${USE_CPP_VERSION})
-endif()
-
-
-#-----------------------------------------------------------------------------
-#
 #  Compiler and Linker flags
 #
 #-----------------------------------------------------------------------------
